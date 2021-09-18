b09108057d87e8a337892bc625f72c231e7272a9
LuiCoro <luiacorona029@icloud.com>
2021-09-17 19:55:26 -0700

    'Final Commit of Hand Holding' - this mean anything that was already there - Next we will be creating this from scratch

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,40 +1,42 @@
 import React, { useReducer } from 'react';
 
-// Step 3
-//import react-reduc after adding the store in index.js ( top level )
 import {connect} from "react-redux";
 
-import titleReducer, { initialState } from '../reducers/titleReducer';
+// import titleReducer, { initialState } from '../reducers/titleReducer';
 import { toggleEditing, updateTitle} from './../actions/titleActions';
 
 import TitleDisplay from './TitleDisplay';
 import TitleForm from './TitleForm';
 
 const Title = (props) => {
-  const [state, dispatch] = useReducer(titleReducer, initialState);
+  // Step 4 Continued
+  //No longer used thanks to props
+  // const [state, dispatch] = useReducer(titleReducer, initialState);
   console.log(props)
 
   const handleToggleEditing = () => {
-    dispatch(toggleEditing());
+    //Step 4 Continued : remember to add 'props' in the front
+    props.dispatch(toggleEditing());
   }
 
   const handleTitleUpdate = (title) => {
-    dispatch(updateTitle(title));
+    props.dispatch(updateTitle(title));
   }
 
   return (
     <div>
-      <h1>{state.appName}</h1>
+      <h1>{props.appName}</h1>
       {
-        !state.editing ? 
-          <TitleDisplay title={state.title} handleToggleEditing={handleToggleEditing}/>: 
+        !props.editing ?
+          <TitleDisplay title={props.title} handleToggleEditing={handleToggleEditing}/>:
           <TitleForm handleTitleUpdate={handleTitleUpdate}/>
       }
     </div>
   );
 };
 
-// Step 3 Continued . after connect we create this
+//Step 4
+// This preety much connects us to our state
 const mapStateToProps = (state) => {
   return ({
     title: state.title,
@@ -43,4 +45,12 @@ const mapStateToProps = (state) => {
   })
 }
 
-export default connect(mapStateToProps)(Title);
+//Step 5:
+const mapDispatchToProps = {
+  updateTitle,
+  toggleEditing
+}
+
+export default connect(mapStateToProps , mapDispatchToProps)(Title);
+
+//If you need to access just your actions pull 'null' as the first parameter!
